dc4cf07472417400d44a012462399a21e47d2616
Chris Jeon <cjeon97@gmail.com>
2020-04-24 00:51:08 -0400

    add State handlers

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText)  => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]); // ... pulls out elems from old array and puts new and old ones into new array
+  }
+
+
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput 
           placeholder="Course Goal" 
           style={styles.input}
+          onChangeText = {goalInputHandler} //dont add parentheses after goalInputHandler
+          value={enteredGoal} //change state here
         />
-        <Button title="ADD"/>
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
       <View>
-
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
 
     </View>
